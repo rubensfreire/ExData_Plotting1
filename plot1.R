8b702fc293e420d04e9fb3d76d29f4aa80e09593
rubensfreire <rubensfr@yahoo.com>
2016-11-12 11:46:39 -0200

    Minor changes

--- plot1.R
+++ plot1.R
@@ -1,6 +1,5 @@
 
 # This function reads household_power_consumption.txt and plots  Global_active_power for 2 days
-
 plot1<-function(){
         print("Reading the file...")
         power_data<-read.csv2("household_power_consumption.txt",na.strings ="?")
